5ef2dfe14f4853ed8a333db81912ca76beded8f0
Mevaser <mevaser.rotner@gmail.com>
2022-06-25 16:43:59 +0300

    Auto generate link_uuid for resources

--- app/db/models/abstract_resource.py
+++ app/db/models/abstract_resource.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from tortoise import fields, models
 
 from app.db.models.timestamp_mixin import TimestampMixin
@@ -5,7 +6,7 @@ from app.db.models.timestamp_mixin import TimestampMixin
 class AbstractResource(TimestampMixin, models.Model):
     id = fields.IntField(pk=True)
     name = fields.CharField(max_length=255)
-    link_uuid = fields.UUIDField(null=True)
+    link_uuid = fields.UUIDField(unique=True, default=uuid4)
 
     class Meta:
         abstract = True
