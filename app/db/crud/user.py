27091022b9e2d92122ee45329b67916802d49d79
Mevaser <mevaser.rotner@gmail.com>
2022-06-25 18:37:53 +0300

    Revert "Stop prefetching of relational data"
    This reverts commit 68a05bed1510a16cc689a28d9341932a41536066.
    
    I wish that would have worked, but it seems that the combination of pydantic + tortoise requires from_tortoise_orm and it fetches everything :/

--- app/db/crud/user.py
+++ app/db/crud/user.py
@@ -4,14 +4,14 @@ class UserDAL:
     @staticmethod
     async def get_or_create(user: schemas.UserType) -> schemas.User:
         db_user, _ = await models.User.get_or_create(**user.dict())
-        return await schemas.User.from_orm(db_user)
+        return await schemas.User.from_tortoise_orm(db_user)
 
     @staticmethod
     async def get_or_none(user: schemas.UserType) -> schemas.User:
         db_user = await models.User.get_or_none(**user.dict())
         if db_user is None:
             return None
-        return await schemas.User.from_orm(db_user)
+        return await schemas.User.from_tortoise_orm(db_user)
 
     @staticmethod
     async def update(filter_args: dict, update_args: dict):
