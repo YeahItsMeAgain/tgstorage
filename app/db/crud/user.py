7ba0c39b4a36c4083d992bef36bbbf8c49d95bbb
Mevaser <mevaser.rotner@gmail.com>
2022-07-11 22:37:01 +0300

    Removed unused function from UserDAL

--- app/db/crud/user.py
+++ app/db/crud/user.py
@@ -11,12 +11,5 @@ class UserDAL:
         return await models.User.get_or_none(**user.dict())
 
     @staticmethod
-    async def get_or_none(user: schemas.UserType) -> schemas.User:
-        db_user = await UserDAL.get_db_model_or_none(user)
-        if db_user is None:
-            return None
-        return await schemas.User.from_tortoise_orm(db_user)
-
-    @staticmethod
     async def update(filter_args: dict, update_args: dict):
         await models.User.select_for_update().filter(**filter_args).update(**update_args)
