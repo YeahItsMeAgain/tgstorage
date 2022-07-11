c88b67977ae58d5ed47be3de0a900128abe11f46
Mevaser <mevaser.rotner@gmail.com>
2022-07-11 22:17:23 +0300

    Seperate connect_bot to 2 functions

--- app/dependencies/connect_bot.py
+++ app/dependencies/connect_bot.py
@@ -7,13 +7,16 @@ from app import redis_connector
 from app.settings import Settings
 from app.dependencies.settings import get_settings
 
+async def get_bot(bot_token: str, user_email: str, settings: Settings):
+    session_name = f'{user_email}_{bot_token}'
+    bot = TelegramClient(RedisSession(session_name, redis_connector), settings.API_ID, settings.API_HASH)
+    if not bot.is_connected():
+        await bot.start(bot_token=bot_token)
+    return bot
+
 async def connect_bot(request: Request, settings: Settings = Depends(get_settings)):
     bot_token = request.session.get('bot_token', None)
     if not bot_token:
         raise AccessTokenInvalidError('Invalid Access token')
 
-    session_name = f'{request.session["email"]}_{bot_token}'
-    bot = TelegramClient(RedisSession(session_name, redis_connector), settings.API_ID, settings.API_HASH)
-    if not bot.is_connected():
-        await bot.start(bot_token=bot_token)
-    return bot
+    return await get_bot(bot_token, request.session["email"], settings)
--- app/routes/index.py
+++ app/routes/index.py
@@ -2,6 +2,7 @@ from typing import Dict
 from fastapi import APIRouter, Depends
 from fastapi_restful.cbv import cbv
 from starlette.requests import Request
+from app.db.schemas.user import SessionUser
 
 from app.dependencies.auth import get_current_user
 
@@ -13,7 +14,7 @@ router = APIRouter(
 
 @cbv(router)
 class Index:
-	user: Dict = Depends(get_current_user)
+	user: SessionUser = Depends(get_current_user)
 
 	@router.get('/')
 	async def index(self, _: Request):
